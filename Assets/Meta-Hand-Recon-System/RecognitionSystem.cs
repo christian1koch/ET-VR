3762c326cefce3ecf449bc06acfbe55c9b3960d9
christian1koch <ko.che@web.de>
2025-11-10 18:05:40 +0100

    Recognized hand insise of the UI

--- Assets/Meta-Hand-Recon-System/RecognitionSystem.cs
+++ Assets/Meta-Hand-Recon-System/RecognitionSystem.cs
@@ -1,16 +1,19 @@
 using System;
 using Oculus.Interaction.PoseDetection;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RecognitionSystem : MonoBehaviour
 {
     public ShapeRecognizer recognizedPose = null;
     public event Action<int> OnRecognized;
+    public UnityEvent<string> onUnityRecognized;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void SetRecognized(ShapeRecognizer recognized)
     {
         recognizedPose = recognized;
         //TODO: Find a better way to do this, something like a list we can add the list of Poses and then each one recieves a index
         OnRecognized?.Invoke(Int32.Parse(recognizedPose.ShapeName));
+        onUnityRecognized?.Invoke(recognizedPose.name);
     }
 }
